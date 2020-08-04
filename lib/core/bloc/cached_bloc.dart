4b4259030779b3205356bdf5e7f8afd67d4bff33
Linh Ngo <thlinh.ngo@gmail.com>
2020-08-04 10:41:53 +0700

    Cached Bloc

---
+++ lib/core/bloc/cached_bloc.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
+import 'package:mg/core/type.dart';
+
+abstract class CachedBloc<T> extends Cubit<T> {
+  final Box cachedBox;
+
+  CachedBloc(T state, this.cachedBox) : super(state);
+
+  @override
+  Future<void> close() async {
+    await cachedBox.close();
+    return super.close();
+  }
+}
+
+abstract class SimpleCachedBloc<T, S> extends CachedBloc<T> {
+  final Box<String> cachedBox;
+
+  SimpleCachedBloc(T initialState, this.cachedBox)
+      : super(initialState, cachedBox);
+
+  Future<void> putValue(S data) {
+    final dataString = jsonEncode(data);
+
+    return cachedBox.put(_Key.cached, dataString);
+  }
+
+  S getValue(FromJson<S> fromJson) {
+    final source = cachedBox.get(_Key.cached);
+
+    if (source?.isEmpty ?? false) {
+      return null;
+    }
+
+    final decodeObject = jsonDecode(source);
+
+    return fromJson(decodeObject);
+  }
+}
+
+class _Key {
+  static const cached = 'cached';
+}
---
+++ lib/core/type.dart
@@ -0,0 +1,3 @@
+typedef FromJson<T> = T Function(Map<String, dynamic> json);
+
+typedef ToJson = Map<String, dynamic> Function();
